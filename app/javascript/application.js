4e7a6a60dd2a33978d945b31db5dce9e212415fa
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2026-03-01 22:34:57 -0600

    Update comment for Turbo Drive configuration

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-// Change to true to allow Turbo
+// Change to true to enable Turbo Drive
 Turbo.session.drive = false
 
 // Allow UJS alongside Turbo
